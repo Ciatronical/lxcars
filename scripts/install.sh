37c8401bf3fdcc6d8d4bd4fb15f2e27c4ac22be0
Ronny Kumke <ronny@lxcars.de>
2012-03-04 19:18:05 +0100

    install.sh verbessert, stdLib gepatcht

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 #set -x
 
+## Begin: an System, Installation anpassen
+DIR_LxCars=/usr/lib/lx-office-crm/lxcars
+DIR_ERP=/usr/lib/lx-office-erp
+## END
+
+
+
 echo "Willkommen bei der LxCars-Installation"
+echo "Verzeichnis LxCars: $DIR_LxCars"
+echo "Verzeichnis ERP: $DIR_ERP"
 sleep 3
 
 echo "************************************************"
@@ -15,16 +24,15 @@ echo "Erzeuge Datenbank lxcars"
 echo "************************************************" 
 ## Datenbank für lxc2db erstellen
 /usr/bin/sudo -u postgres createdb lxcars
-cd .. #nach lxcars wechseln
 if [ $(uname -i) == x86_64 ]; then
    echo "64Bit-Version wird installiert"
-   ln -s lxc2db-x86_64 lxc2db
+   ln -s $DIR_LxCars/lxc2db-x86_64 $DIR_LxCars/lxc2db
 else
    echo "32Bit-Version wird installiert"
-   ln -s lxc2db-i386 lxc2db
+   ln -s $DIR_LxCars/lxc2db-i386 $DIR_LxCars/lxc2db
 fi
 
-/usr/bin/sudo -u www-data ./lxc2db -d lxcars -i
+/usr/bin/sudo -u www-data $DIR_LxCars/lxc2db -d lxcars -i
 
 
 ## Prufen ob schon *.orig-Files existieren
@@ -32,11 +40,11 @@ fi
 ## fur selbige - Links zu den entsprechenden von LxCars erweiterten Dateien erzeugen 
 
 ## Menue erstellen 
-if [ -f /usr/lib/lx-office-erp/menu.ini.orig ]; then
+if [ -f $DIR_ERP/menu.ini.orig ]; then
    echo "Error menue.ini.orig existiert bereits"
 else
- 	mv /usr/lib/lx-office-erp/menu.ini /usr/lib/lx-office-erp/menu.ini.orig
-	ln -s /usr/lib/lx-office-crm/lxcars/lx-office-erp/menu.ini  /usr/lib/lx-office-erp/menu.ini
+ 	mv $DIR_ERP/menu.ini $DIR_ERP/menu.ini.orig
+	ln -s $DIR_LxCars/lx-office-erp/menu.ini  $DIR_ERP/menu.ini
 	echo "menue.ini als menue.ini.oirg gesichert"
 fi
 
@@ -46,30 +54,29 @@ fi
 # 	echo "Error firmen3.tpl.orig existiert bereits"
 # 	else
 # 		mv /usr/lib/lx-office-crm/tpl/firmen3.tpl /usr/lib/lx-office-crm/tpl/firmen3.tpl.orig
-#		ln -s /usr/lib/lx-office-crm/lxcars/tpl/firmen3.tpl  /usr/lib/lx-office-crm/tpl/firmen3.tpl 
+#		ln -s $DIR_LxCars/tpl/firmen3.tpl  /usr/lib/lx-office-crm/tpl/firmen3.tpl 
 #	echo "firmen3.tpl als firmen3.tpl.orig gesichert"
 # fi
 
 ## Logo
-if [ -f /usr/lib/lx-office-erp/image/lx-office-erp.png.orig ]; then
+if [ -f $DIR_ERP/image/lx-office-erp.png.orig ]; then
  	echo "Error lx-office-erp.png.orig existiert bereits"
 else
- 	mv /usr/lib/lx-office-erp/image/lx-office-erp.png /usr/lib/lx-office-erp/image/lx-office-erp.png.orig
-	ln -s /usr/lib/lx-office-crm/lxcars/image/lx-office-erp.png /usr/lib/lx-office-erp/image/lx-office-erp.png
+ 	mv $DIR_ERP/image/lx-office-erp.png $DIR_ERP/image/lx-office-erp.png.orig
+	ln -s $DIR_LxCars/image/lx-office-erp.png $DIR_ERP/image/lx-office-erp.png
 	echo "lx-office-erp.png als lx-office-erp.png.orig gesichert"
 fi 
  
 ## Bilchen furs Menu
-cp /usr/lib/lx-office-crm/lxcars/image/icons/16x16/*  /usr/lib/lx-office-erp/image/icons/16x16/
-cp /usr/lib/lx-office-crm/lxcars/image/icons/24x24/*  /usr/lib/lx-office-erp/image/icons/24x24/
-cp /usr/lib/lx-office-crm/lxcars/image/icons/32x32/*  /usr/lib/lx-office-erp/image/icons/32x32/
+cp $DIR_LxCars/image/icons/16x16/*  $DIR_ERP/image/icons/16x16/
+cp $DIR_LxCars/image/icons/24x24/*  $DIR_ERP/image/icons/24x24/
+cp $DIR_LxCars/image/icons/32x32/*  $DIR_ERP/image/icons/32x32/
 
-chown -R www-data: /usr/lib/lx-office-crm/lxcars
+chown -R www-data: $DIR_LxCars
 
 ## Kfz-Button in Kundenmaske
-cd .. # /usr/lib/lx-office-crm
-patch -p1 < lxcars/lxc-misc/button.patch -b
-
+patch -p1 < $DIR_LxCars/lxc-misc/button.patch -b
+patch -p1 < $DIR_LxCars/lxc-misc/stdLib.patch -b
  
 echo "done!!"
 
--- scripts/remove.sh
+++ scripts/remove.sh
@@ -1,7 +1,13 @@
 #!/bin/sh
 set -e
+
+DIR_CRM=/usr/lib/lx-office-crm
+DIR_ERP=/usr/lib/lx-office-erp
+
 echo "*******************************************" 
 echo "Deinstallation LxCars."
+echo "Verzeichnis CRM: $DIR_CRM"
+echo "Verzeichnis ERP: $DIR_ERP"
 echo "*******************************************"
 /usr/bin/sudo -u postgres dropdb lxcars
 echo "Datenbankuser www-data wird gelöscht."
@@ -11,55 +17,64 @@ echo "*******************************************"
 
 #Links löschen und *.orig in * umbenennen
 ## Menü löschen
-if [ -f /usr/lib/lx-office-erp/menu.ini.orig ]; then
-   rm /usr/lib/lx-office-erp/menu.ini
-   mv /usr/lib/lx-office-erp/menu.ini.orig /usr/lib/lx-office-erp/menu.ini
+if [ -f $DIR_ERP/menu.ini.orig ]; then
+   rm $DIR_ERP/menu.ini
+   mv $DIR_ERP/menu.ini.orig $DIR_ERP/menu.ini
 else
    echo "Error menue.ini.orig nicht gefunden"
 fi
  
-#if [ -f /usr/lib/lx-office-crm/tpl/firmen3.tpl.orig ]; then
-#  rm /usr/lib/lx-office-crm/tpl/firmen3.tpl
-#  mv /usr/lib/lx-office-crm/tpl/firmen3.tpl.orig /usr/lib/lx-office-crm/tpl/firmen3.tpl
+#if [ -f $DIR_CRM/tpl/firmen3.tpl.orig ]; then
+#  rm $DIR_CRM/tpl/firmen3.tpl
+#  mv $DIR_CRM/tpl/firmen3.tpl.orig $DIR_CRM/tpl/firmen3.tpl
 #else
 #  echo "Error firmen3.tpl.orig nicht gefunden"
 #fi 
 
 ## Bild löschen 
-if [ -f /usr/lib/lx-office-erp/image/lx-office-erp.png.orig ]; then
-   rm /usr/lib/lx-office-erp/image/lx-office-erp.png  
-   mv /usr/lib/lx-office-erp/image/lx-office-erp.png.orig /usr/lib/lx-office-erp/image/lx-office-erp.png
+if [ -f $DIR_ERP/image/lx-office-erp.png.orig ]; then
+   rm $DIR_ERP/image/lx-office-erp.png  
+   mv $DIR_ERP/image/lx-office-erp.png.orig $DIR_ERP/image/lx-office-erp.png
 else
    echo "Error lx-office-erp.png.orig nicht gefunden"
 fi 
  
 ## Icons löschen
-if [ -f /usr/lib/lx-office-erp/image/icons/16x16/LxCars--Schnellsuche.png ]; then
-   rm /usr/lib/lx-office-erp/image/icons/16x16/LxCars*  
+if [ -f $DIR_ERP/image/icons/16x16/LxCars--Schnellsuche.png ]; then
+   rm $DIR_ERP/image/icons/16x16/LxCars*  
 else
-   echo "Error /usr/lib/lx-office-erp/image/icons/16x16/LxCars* nicht gefunden"
+   echo "Error $DIR_ERP/image/icons/16x16/LxCars* nicht gefunden"
 fi
-if [ -f /usr/lib/lx-office-erp/image/icons/24x24/LxCars--Schnellsuche.png ]; then
-   rm /usr/lib/lx-office-erp/image/icons/24x24/LxCars*  
+if [ -f $DIR_ERP/image/icons/24x24/LxCars--Schnellsuche.png ]; then
+   rm $DIR_ERP/image/icons/24x24/LxCars*  
 else
-   echo "Error /usr/lib/lx-office-erp/image/icons/24x24/LxCars* nicht gefunden"
+   echo "Error $DIR_ERP/image/icons/24x24/LxCars* nicht gefunden"
 fi
-if [ -f /usr/lib/lx-office-erp/image/icons/32x32/LxCars--Schnellsuche.png ]; then
-   rm /usr/lib/lx-office-erp/image/icons/32x32/LxCars*  
+if [ -f $DIR_ERP/image/icons/32x32/LxCars--Schnellsuche.png ]; then
+   rm $DIR_ERP/image/icons/32x32/LxCars*  
 else
-   echo "Error /usr/lib/lx-office-erp/image/icons/32x32/LxCars* nicht gefunden"
+   echo "Error $DIR_ERP/image/icons/32x32/LxCars* nicht gefunden"
 fi
 
-## Patch rückgängig machen
-if [ -f /usr/lib/lx-office-crm/tpl/firma1.tpl.orig ]; then
-   rm /usr/lib/lx-office-crm/tpl/firma1.tpl
- 	mv /usr/lib/lx-office-crm/tpl/firma1.tpl.orig /usr/lib/lx-office-crm/tpl/firma1.tpl
+## Patch für Button rückgängig machen
+if [ -f $DIR_CRM/tpl/firma1.tpl.orig ]; then
+   rm $DIR_CRM/tpl/firma1.tpl
+   mv $DIR_CRM/tpl/firma1.tpl.orig $DIR_CRM/tpl/firma1.tpl
 else
    echo "Error firma1.tpl.orig nicht gefunden"
 fi 
 
+## Patch für stdlib rückgängig machen
+if [ -f $DIR_CRM/inc/stdLib.php.orig ]; then
+   rm $DIR_CRM/inc/stdLib.php
+   mv $DIR_CRM/inc/stdLib.php.orig $DIR_CRM/inc/stdLib.php
+else
+   echo "Error stdLib.php.orig nicht gefunden"
+fi 
+
+
 ## Link für Architektur löschen
-rm /usr/lib/lx-office-crm/lxcars/lxc2db
+rm $DIR_CRM/lxcars/lxc2db
 
 
 echo "done!!"
