48d4b770c58ebaf60e99673eda3b2ce48aa6f107
Jens Heine <jens@heine-bb.de>
2016-09-02 17:09:56 +0200

    Fertigstellung der Funktion zum holen der Auftragsinformationen

--- ajax/order.php
+++ ajax/order.php
@@ -15,12 +15,11 @@ function newEntry( $data ){
 
 function getOrder($id){
 writeLog($id);
-     $rs = $GLOBALS['dbh']->getOne( 'SELECT * FROM lxc_a WHERE lxc_a_id = '.$id, true );
+     $rs = $GLOBALS['dbh']->getOne( 'SELECT lxc_a_id, name AS kundenname, lxc_a_finish_time, lxc_a_init_time, lxc_a_km, lxc_a_modified_on, c_ln FROM lxc_a, lxc_cars, customer WHERE lxc_a_id = '.$id.' AND lxc_a_c_id = c_id AND c_ow = id', true );
      //echo json_encode( $rs['json_agg'] );
      //echo json_encode( $rs );
      echo $rs;
 }
-
 
 function getArtikel($id){
      $rs = $GLOBALS['dbh']->getAll( 'SELECT * FROM lxc_a_pos WHERE lxc_a_pos_aid = '.$id, true );
@@ -28,38 +27,6 @@ function getArtikel($id){
          //echo json_encode( $rs );
      echo $rs;
 }
-
-/*
-// Gibt ein Array mit allen Nutzern einer angegebenen Grupppe zurück
-function ERPUsersfromGroup($grp_name) {
-    $rueck;
-    $i = 0;
-    $grp_id = '';
-    //Gruppen ID herausfiltern
-    $allERPusers = getAllERPusers();
-    $allERPgroups = getAllERPgroups();
-    $sql = "SELECT usrg.user_id AS user_id, usrg.group_id AS group_id FROM auth.user_group AS usrg ORDER by usrg.user_id";
-    $allAssignments = $GLOBALS['dbh_auth']->getAll( $sql );
-    foreach ( $allERPgroups as $key => $gruppe ) {
-        if($gruppe['name'] == $grp_name) {
-            $grp_id = $gruppe['id'];
-        }
-    }
-    //Rückgabe-Array zusammensetzeng
-    foreach ( $allAssignments as $key => $zuordnung ) {
-        if($zuordnung['group_id'] == $grp_id) {
-            $user_id = $zuordnung['user_id'];
-            foreach ( $allERPusers as $key => $nutzer ) {
-                    if($nutzer['id'] == $user_id) {
-                        $rueck[$i] = array("id"=>$user_id,"login"=>$nutzer['login'],"name"=>$nutzer['name']);
-                        $i++;
-                    }
-            }
-        }
-    }
-    return $rueck;
-}
-*/
 
 /*
 function getArtikel($artikel){
