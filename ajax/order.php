314ce7cc5cf1c5ddf06cd22399082c54f56811da
Jens Heine <jens@heine-bb.de>
2016-09-13 16:35:52 +0200

    Update-Funktion fertiggestellt 1. bei Eingabe von Änderungen 2. Bei verschieben von Positionen
    holen der PositionsID nach dem Insert

--- ajax/order.php
+++ ajax/order.php
@@ -2,25 +2,36 @@
 //writeLog(__DIR__);
 require_once __DIR__.'/../inc/ajax2function.php';
 
-function newEntry($data){
-    writeLog( 'Ronny' );
+function newEntry( $data ){
+    //writeLog( 'Ronny' );
     //writeLog( $data );
-    $data = json_decode($data);
+    $data = json_decode( $data );
     //writeLog($data);
-    $data = (array) $data;
-    writeLog($data);
-    $rs = $GLOBALS['dbh']->insert( 'lxc_a_pos', array( 'lxc_a_pos_aid', 'lxc_a_pos_order_nr', 'lxc_a_pos_todo', 'lxc_a_pos_emp', 'lxc_a_pos_status' ), array( $data['lxc_a_pos_aid'], $data['lxc_a_pos_order_nr'], $data['lxc_a_pos_todo'],$data['lxc_a_pos_emp'], $data['lxc_a_pos_status']), 'lxc_a_pos_id' );
-    writelog($rs);
+    $data = ( array ) $data;
+    writeLog( $data );
+    $rs = $GLOBALS[ 'dbh' ]->insert( 'lxc_a_pos', array( 'lxc_a_pos_aid', 'lxc_a_pos_order_nr', 'lxc_a_pos_todo', 'lxc_a_pos_emp', 'lxc_a_pos_status' ), array( $data['lxc_a_pos_aid'], $data['lxc_a_pos_order_nr'], $data['lxc_a_pos_todo'],$data['lxc_a_pos_emp'], $data['lxc_a_pos_status']), 'lxc_a_pos_id' );
+    writelog( $rs );
     echo 1;
 }
 
 function updatePositions( $data) {
-    //writeLog( 'Ronny' );
-    writeLog($data);
-    //$data = json_decode($data);
-    //writeLog($data);
+    writeLog( $data );
+    //$GLOBALS['dbh']->begin();
+
+    foreach( $data as $key => $value ){
+        writeLog( $value );
+        $GLOBALS['dbh']->update( 'lxc_a_pos', array('lxc_a_pos_order_nr', 'lxc_a_pos_todo', 'lxc_a_pos_emp', 'lxc_a_pos_status'), array($value['lxc_a_pos_order_nr'], $value['lxc_a_pos_todo'], $value['lxc_a_pos_emp'], $value['lxc_a_pos_status']), 'lxc_a_pos_id = '.$value['lxc_a_pos_id'] );
+    }
+
+    //$data = ( array ) $data;
+    //writeLog( $data );
+
+    //$rs = $GLOBALS['dbh']->update( 'lxc_a_pos', array( 'lxc_a_pos_todo' ), array( $data['lxc_a_pos_todo'] ), "lxc_a_pos_id = ".$data['lxc_a_pos_id'] );
+
 
     //Begin
+
+
         //$result = parent::beginTransaction();
         //if( $this->logAll ) $this->writeLog( "PDO::beginTransaction() returns: ".$result );
             //return $result;
@@ -36,7 +47,7 @@ function updatePositions( $data) {
 }
 
 function getOrder($id){
-writeLog($id);
+     writeLog($id);
      $rs = $GLOBALS['dbh']->getOne( 'SELECT lxc_a_id, name AS kundenname, lxc_a_finish_time, lxc_a_init_time, lxc_a_km, lxc_a_modified_on, c_ln FROM lxc_a, lxc_cars, customer WHERE lxc_a_id = '.$id.' AND lxc_a_c_id = c_id AND c_ow = id', true );
      //echo json_encode( $rs['json_agg'] );
      //echo json_encode( $rs );
@@ -48,6 +59,13 @@ function getPosition($id){
          //echo json_encode( $rs['json_agg'] );
          //echo json_encode( $rs );
      echo $rs;
+}
+
+function delPosition( $data ){
+    writeLog($data);
+    //$sql = "DELETE FROM lxc_a_pos WHERE lxc_a_pos_id = '".$data."'";
+    //$rs = $GLOBALS['dbh']->query( $sql );
+    echo 1;
 }
 
 
