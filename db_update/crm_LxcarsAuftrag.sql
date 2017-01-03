b9bebbe4e6e0998aba99021f900e89ef455116c7
Ronny Kumke <ronny@lxcars.de>
2017-01-03 19:32:08 +0100

    Order: Date-Time-Picker hinzugefügt, Spalte finish_time in Text geändert

--- db_update/crm_LxcarsAuftrag.sql
+++ db_update/crm_LxcarsAuftrag.sql
@@ -6,7 +6,7 @@ ALTER TABLE oe ADD km_stnd INT;
 ALTER TABLE oe ADD c_id INT;
 ALTER TABLE oe ADD status TEXT;
 ALTER TABLE oe ADD car_status TEXT;
-ALTER TABLE oe ADD finish_time TIMESTAMP;
+ALTER TABLE oe ADD finish_time TEXT;
 
 ALTER TABLE orderitems ADD status TEXT;
 ALTER TABLE orderitems ADD u_id TEXT;
