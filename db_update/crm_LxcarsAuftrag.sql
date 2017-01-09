8f5ee84b31cbe1dd3a7d441a6a697c44e9774716
Jens Heine <jens@heine-bb.de>
2017-01-09 14:33:41 +0100

    Order: Zeile vergessen, Groß-Klein-Schreibung Jens..

--- db_update/crm_LxcarsAuftrag.sql
+++ db_update/crm_LxcarsAuftrag.sql
@@ -11,6 +11,7 @@ ALTER TABLE oe ADD finish_time TEXT;
 ALTER TABLE orderitems ADD status TEXT;
 ALTER TABLE orderitems ADD u_id TEXT;
 
---ALTER TABLE oe ALTER ColUMN km_stnd SET DEFAULT 0;
+--ALTER TABLE oe ALTER COLUMN km_stnd SET DEFAULT 0;
+--ALTER TABLE oe ADD finish_time TEXT;
 
 -- @exec
