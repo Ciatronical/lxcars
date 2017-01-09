85e401fb1ee6e11ddd14d6f1eaa4cfe92acc4cf6
Jens Heine <jens@heine-bb.de>
2017-01-09 14:16:45 +0100

    Order: KM-Stand in DB 0

--- db_update/crm_LxcarsAuftrag.sql
+++ db_update/crm_LxcarsAuftrag.sql
@@ -2,7 +2,7 @@
 -- @description: Add columns to tables
 -- @version: 2.3.1
 
-ALTER TABLE oe ADD km_stnd INT;
+ALTER TABLE oe ADD km_stnd INT DEFAULT 0;
 ALTER TABLE oe ADD c_id INT;
 ALTER TABLE oe ADD status TEXT;
 ALTER TABLE oe ADD car_status TEXT;
@@ -10,5 +10,7 @@ ALTER TABLE oe ADD finish_time TEXT;
 
 ALTER TABLE orderitems ADD status TEXT;
 ALTER TABLE orderitems ADD u_id TEXT;
+
+--ALTER TABLE oe ALTER ColUMN km_stnd SET DEFAULT 0;
 
 -- @exec
