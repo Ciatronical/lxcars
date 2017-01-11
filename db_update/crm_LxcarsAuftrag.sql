24db484fd2c5c5534f251ca7cf742a2114a9b2b3
Ronny Kumke <ronny@lxcars.de>
2017-01-11 05:36:36 +0100

    Order: Tabelle Instruktionen hinzugefügt
    Die Instruktionen werden zusammen mit den Orderitems dargestellt und können
    via sortable() sortiert werden. In orderitems.positions werden Lücken entstehen.
    Nach jeder Instuktion kommt mind. ein Orderitem bzw die Autocompletion-Line.
    Hinter der letzen Orderitem-Autocompletion-Line befindet sich Instruction-Line.
    Die Instruction-Lines werden via CSS z.B. rot gefärbt.
    Gedruckt werden ausschließlich die Instruction-Lines.

--- db_update/crm_LxcarsAuftrag.sql
+++ db_update/crm_LxcarsAuftrag.sql
@@ -8,8 +8,26 @@ ALTER TABLE oe ADD status TEXT;
 ALTER TABLE oe ADD car_status TEXT;
 ALTER TABLE oe ADD finish_time TEXT;
 
-ALTER TABLE orderitems ADD status TEXT;
-ALTER TABLE orderitems ADD u_id TEXT;
+CREATE  TABLE instructions(
+    id serial,
+    trans_id integer,
+    parts_id integer,
+    description text,
+    longdescription text,
+    sellprice numeric(15,5),
+    itime timestamp without time zone DEFAULT now(),
+    mtime timestamp without time zone,
+    ordnumber text,
+    "position" integer NOT NULL,
+    status text,
+    u_id text,
+    employee text,
+    dictated_during tsrange,
+    needed_during tsrange
+);
+
+--ALTER TABLE orderitems ADD status TEXT;
+--ALTER TABLE orderitems ADD u_id TEXT;
 
 --ALTER TABLE oe ALTER COLUMN km_stnd SET DEFAULT 0;
 --ALTER TABLE oe ADD finish_time TEXT;
