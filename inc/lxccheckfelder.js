88f5e7235f701de5795f6f6d802272e43af181ce
Ronny Zimmermann <ronny@inter-data.de>
2017-07-17 11:31:57 +0200

    Link special geändert

--- inc/lxccheckfelder.js
+++ inc/lxccheckfelder.js
@@ -51,7 +51,7 @@ function lxc_auf( c_id, owner, task ){
     location.href=uri;
 }
 function special( c_id, owner, task ){
-    uri1="mytmp/lxcars/special.phtml?c_id=" +  c_id;
+    uri1="special/special.phtml?c_id=" +  c_id;
     uri2="&owner=" + owner;
     uri3="&task=" + task;
     uri=uri1+uri2+uri3;
