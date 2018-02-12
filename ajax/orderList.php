5630b59e130f6bae7cc6c76ce080dd36e9b3d13f
Ronny Zimmermann <ronny@inter-data.de>
2018-02-12 03:41:19 +0100

    Auftragsliste: DB-Abfrage Betrag ist nicht mehr NULL

--- ajax/orderList.php
+++ ajax/orderList.php
@@ -5,17 +5,15 @@ require_once __DIR__.'/../../inc/crmLib.php';
 require_once __DIR__.'/../inc/ajax2function.php';
 
 function getOrderList( $data ){
-  
-    $format = "'DD.MM.YYYY HH24:MI'";
 
-    $sql = "SELECT distinct on ( ordnumber ) * FROM ( SELECT distinct on (oe.ordnumber) 'true'::BOOL AS instruction,oe.id, to_char(oe.itime, ".$format.") AS itime, to_char(oe.mtime, ".$format.") AS mtime, oe.ordnumber, instructions.description, oe.amount ";
+    $format = "'DD.MM.YYYY HH24:MI'"; //ToDo lng
+
+    $sql = "SELECT distinct on ( ordnumber ) * FROM ( SELECT distinct on ( oe.ordnumber ) 'true'::BOOL AS instruction, oe.id, to_char( oe.itime, ".$format." ) AS itime, to_char(oe.mtime, ".$format." ) AS mtime, oe.ordnumber, instructions.description, COALESCE( NULLIF( oe.amount, null ) , 0 ) AS amount ";
     $sql.= "FROM oe, instructions, parts WHERE oe.customer_id = ".$data['owner']." AND instructions.trans_id = oe.id AND oe.c_id = ".$data['c_id']." AND parts.id = instructions.parts_id AND instructions.position = 1 UNION ";
-    $sql.= "SELECT distinct on (oe.ordnumber)'false'::BOOL AS instruction,oe.id, to_char(oe.itime, ".$format.") AS itime, to_char(oe.mtime, ".$format.") AS mtime, oe.ordnumber, orderitems.description, oe.amount FROM oe, orderitems, parts ";
+    $sql.= "SELECT distinct on ( oe.ordnumber )'false'::BOOL AS instruction,oe.id, to_char( oe.itime, ".$format." ) AS itime, to_char( oe.mtime, ".$format." ) AS mtime, oe.ordnumber, orderitems.description, COALESCE( NULLIF( oe.amount, null ) , 0 ) AS amount FROM oe, orderitems, parts ";
     $sql.= "WHERE oe.customer_id = ".$data['owner']." AND oe.c_id = ".$data['c_id']." AND orderitems.trans_id = oe.id AND parts.id = orderitems.parts_id AND orderitems.position = 1 ";
-    $sql.= "ORDER BY instruction DESC ) AS testTable ORDER BY ordnumber DESC";  
-    
+    $sql.= "ORDER BY instruction DESC ) AS testTable ORDER BY ordnumber DESC";
     echo $GLOBALS['dbh']->getALL( $sql, true );
-  
-  }
+}
 
 ?>
