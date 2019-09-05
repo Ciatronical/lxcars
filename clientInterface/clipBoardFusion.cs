b54252d835f83c05084035172ecc064da0c78a45
Ronny Zimmermann <ronny@inter-data.de>
2019-09-05 09:09:03 +0200

    Bei STRG + LEER Leerzeichen entfernen

---
+++ clientInterface/clipBoardFusion.cs
@@ -0,0 +1,9 @@
+using System;
+using System.Collections.Generic;
+// STRG + LEER
+public static class ClipboardFusionHelper{
+    public static string ProcessText( string text ){
+        text = text.Replace( " ", "" );
+        return text;
+    }
+}
