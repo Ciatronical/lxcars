c11224e3291ddd9c3f46a4690315be31baf0897b
Ronny Zimmermann <ronny@lxcars.de>
2019-09-17 22:44:52 +0200

    Orderlist: Aufträge nicht kundenbezogen sondern ausschließlich fahrzeugbezugen anzeigen

--- ajax/orderList.php
+++ ajax/orderList.php
@@ -9,9 +9,9 @@ function getOrderList( $data ){
     $format = "'DD.MM.YYYY HH24:MI'"; //ToDo lng
 
     $sql = "SELECT distinct on ( ordnumber ) * FROM ( SELECT distinct on ( oe.ordnumber ) 'true'::BOOL AS instruction, oe.id, to_char( oe.itime, ".$format." ) AS itime, to_char(oe.mtime, ".$format." ) AS mtime, oe.ordnumber, instructions.description, COALESCE( NULLIF( oe.amount, null ) , 0 ) AS amount ";
-    $sql.= "FROM oe, instructions, parts WHERE oe.customer_id = ".$data['owner']." AND instructions.trans_id = oe.id AND oe.c_id = ".$data['c_id']." AND parts.id = instructions.parts_id AND instructions.position = 1 UNION ";
+    $sql.= "FROM oe, instructions, parts WHERE instructions.trans_id = oe.id AND oe.c_id = ".$data['c_id']." AND parts.id = instructions.parts_id AND instructions.position = 1 UNION ";
     $sql.= "SELECT distinct on ( oe.ordnumber )'false'::BOOL AS instruction,oe.id, to_char( oe.itime, ".$format." ) AS itime, to_char( oe.mtime, ".$format." ) AS mtime, oe.ordnumber, orderitems.description, COALESCE( NULLIF( oe.amount, null ) , 0 ) AS amount FROM oe, orderitems, parts ";
-    $sql.= "WHERE oe.customer_id = ".$data['owner']." AND oe.c_id = ".$data['c_id']." AND orderitems.trans_id = oe.id AND parts.id = orderitems.parts_id AND orderitems.position = 1 ";
+    $sql.= "WHERE oe.c_id = ".$data['c_id']." AND orderitems.trans_id = oe.id AND parts.id = orderitems.parts_id AND orderitems.position = 1 ";
     $sql.= "ORDER BY instruction DESC ) AS testTable ORDER BY ordnumber DESC";
     echo $GLOBALS['dbh']->getALL( $sql, true );
 }
