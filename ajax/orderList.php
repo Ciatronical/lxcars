9cffc8a98f0079f8303bbe576f79c43a7239fd80
Ronny Zimmermann <ronny@inter-data.de>
2021-01-11 13:23:53 +0100

    OrderList: Sortierung nach init-Time

--- ajax/orderList.php
+++ ajax/orderList.php
@@ -8,11 +8,12 @@ function getOrderList( $data ){
 
     $format = "'DD.MM.YYYY HH24:MI'"; //ToDo lng
 
-    $sql = "SELECT distinct on ( id ) * FROM ( SELECT distinct on ( oe.id ) 'true'::BOOL AS instruction, oe.id, to_char( oe.itime, ".$format." ) AS itime, to_char(oe.mtime, ".$format." ) AS mtime, oe.ordnumber, instructions.description, COALESCE( NULLIF( oe.amount, null ) , 0 ) AS amount ";
+    $sql = "SELECT distinct on ( sort_itime ) * FROM ( SELECT distinct on ( oe.id ) 'true'::BOOL AS instruction, oe.id, to_char( oe.itime, ".$format." ) AS itime, to_char(oe.mtime, ".$format." ) AS mtime, oe.ordnumber, instructions.description, COALESCE( NULLIF( oe.amount, null ) , 0 ) AS amount, oe.itime AS sort_itime ";
     $sql.= "FROM oe, instructions, parts WHERE instructions.trans_id = oe.id AND oe.c_id = ".$data['c_id']." AND parts.id = instructions.parts_id AND instructions.position = 1 UNION ";
-    $sql.= "SELECT distinct on ( oe.ordnumber )'false'::BOOL AS instruction, oe.id, to_char( oe.itime, ".$format." ) AS itime, to_char( oe.mtime, ".$format." ) AS mtime, oe.ordnumber, orderitems.description, COALESCE( NULLIF( oe.amount, null ) , 0 ) AS amount FROM oe, orderitems, parts ";
+    $sql.= "SELECT distinct on ( oe.ordnumber )'false'::BOOL AS instruction, oe.id, to_char( oe.itime, ".$format." ) AS itime, to_char( oe.mtime, ".$format." ) AS mtime, oe.ordnumber, orderitems.description, COALESCE( NULLIF( oe.amount, null ) , 0 ) AS amount, oe.itime AS sort_itime FROM oe, orderitems, parts ";
     $sql.= "WHERE oe.c_id = ".$data['c_id']." AND orderitems.trans_id = oe.id AND parts.id = orderitems.parts_id AND orderitems.position = 1 ";
-    $sql.= "ORDER BY instruction DESC ) AS testTable ORDER BY id DESC";
+    $sql.= "ORDER BY instruction DESC ) AS testTable ORDER BY sort_itime DESC";
+    writeLog( $sql );
     echo $GLOBALS['dbh']->getALL( $sql, true );
 }
 
