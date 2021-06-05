86e746fc12f2b0e212d309253d6e6e3515e5f4ec
Ronny Zimmermann <ronny@inter-data.de>
2021-06-05 17:10:40 +0200

    getKBA für Autos fertig
    ToDo: für Roller, Anhänger, LKW, Busse

--- kba/getKBA.php
+++ kba/getKBA.php
@@ -1,5 +1,19 @@
 <?php
-    echo "\n";
+
+    $debug = true;
+
+    function strposArray( $haystack, $needle, $offset = 0 ){
+      if( !is_array( $needle) ) return false;
+      foreach( $needle as $query ){
+          $result  = strpos( $haystack, $query, $offset );
+          if( $result !== false ) return $result;
+      }
+      return false;
+    }
+
+    $carTypesArray = array( 'BEETLE', 'CALIFORNIA', 'CADDY', 'Club', 'Crafter', 'GOLF', 'EOS', 'FOX', 'Hymer', 'JETTA', 'KOMBI', 'PASSAT', 'PHAETON', 'POLO', 'SCIROCCO', 'SHARAN', 'TIGUAN', 'TOURAN', 'VWUP!', 'XL1', 'CC' );
+
+    echo " \n";
     //system( "wget 'https://www.kba.de/SharedDocs/Publikationen/DE/Fahrzeugtechnik/SV/sv42_pdf.pdf?__blob=publicationFile&v=21' -O sv42.pdf" );
     system( 'pdftotext -layout sv42.pdf' );
     //// Alternativ: python3 /usr/local/bin/pdf2txt.py   sv42.pdf > test.txt
@@ -12,18 +26,25 @@
         if( !$goodLine ) unset( $allLines[$key] );
         //print_r( $allLines[$key] );
     }
-    $formatedLines = array();
+    if( $debug ) $formatedLines = array();
     foreach( $allLines as $key => $value ){
-        //if(  !ctype_space( substr( $value, 52, 1 ) ) ) $value = substr_replace( $value, ' ', 52 );
-        //if( !ctype_space( substr( $value, 53, 2 ) ) ) echo( substr( $value, 53, 26 )." key: ".$key."\n");
-        // Submarke substr( $value, 52, 18 );
-        //if( strpos( substr( $value, 70, )))
         if( !ctype_space( substr( $value, 53, 2 ) ) ){ //Kfz-Typen sind vorhanden
             if( substr( $value, 74, 12 ) && !ctype_space( substr( $value, 74, 8 ) ) ){ //sind Kfz-Typen zu weit nach lins verschoben?
-                $posbegin = strpos( $value, "GOLF")
+                $posBegin = strposArray( $value, $carTypesArray, 74 );
+                if( $posBegin === false || $posBegin >= 84 ) continue;
+                $posEnd = strpos( $value, '   ', $posBegin );
+                $length = $posEnd - $posBegin;
+                $stringToShift = substr( $value, $posBegin, $length );
+                $replaceStr = str_repeat( ' ', $length );
+                $value = substr_replace( $value, $replaceStr, $posBegin, $length );
+                $value = substr_replace( $value, $stringToShift, 84, $length );
+                if( $debug ) $formatedLines[$key] = substr( $value, 52, 58 );
+                $allLines[$key] = $value;
+                if( $debug ) echo "posBegin: ".$posBegin." PosEnd: ".$posEnd." String: ".$stringToShift." \n";
             }
         }
-        if( !ctype_space( substr( $value, 53, 2 ) ) ) $formatedLines[$key] = substr( $value, 74, 12 );//52,32
+        //if( !ctype_space( substr( $value, 53, 2 ) ) ) $formatedLines[$key] = substr( $value, 52, 45 );//52,32
+        //if( !ctype_space( substr( $value, 53, 2 ) ) ) $formatedLines[$key] = substr( $value, 74, 12 );//52,32
         //file_put_contents( 'mykba.txt', implode( PHP_EOL, $value ), FILE_APPEND );
 
         ////// also der Trick ist gezielt nach Fhz-Typen zu suchen, Fhz-Typen befinden sich in einem Array
@@ -31,7 +52,7 @@
 
     }
     file_put_contents('mykba.txt', implode( PHP_EOL, $allLines ) );
-    file_put_contents('formatedLines.txt', implode( PHP_EOL, $formatedLines ) );
+    if( $debug ) file_put_contents('formatedLines.txt', implode( PHP_EOL, $formatedLines ) );
     echo "\n\n";
     //var_dump( $lines );
-?>yy
+?>
