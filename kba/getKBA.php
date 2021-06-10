ad4c0ce6d1ddacbcf887424768cc641179f2b314
Ronny Zimmermann <ronny@inter-data.de>
2021-06-10 07:15:00 +0200

    backup

--- kba/getKBA.php
+++ kba/getKBA.php
@@ -10,8 +10,7 @@
       }
       return false;
     }
-
-    $carTypesArray = array( 'BEETLE', 'CALIFORNIA', 'CADDY', 'Club', 'Crafter', 'GOLF', 'EOS', 'FOX', 'Hymer', 'JETTA', 'KOMBI', 'PASSAT', 'PHAETON', 'POLO', 'SCIROCCO', 'SHARAN', 'TIGUAN', 'TOURAN', 'VWUP!', 'XL1', 'CC' );
+    $carTypesArray = array( 'BEETLE', 'CALIFORNIA', 'CADDY', 'Club', 'Crafter', 'GOLF', 'EOS', 'FOX', 'Hymer', 'JETTA', 'KOMBI', 'PASSAT', 'PHAETON', 'POLO', 'SCIROCCO', 'SHARAN', 'TIGUAN', 'TOUAREG', 'TOURAN', 'VWUP!', 'XL1', 'CC' );
 
     echo " \n";
     //system( "wget 'https://www.kba.de/SharedDocs/Publikationen/DE/Fahrzeugtechnik/SV/sv42_pdf.pdf?__blob=publicationFile&v=21' -O sv42.pdf" );
@@ -28,30 +27,35 @@
     }
     if( $debug ) $formatedLines = array();
     foreach( $allLines as $key => $value ){
-        if( !ctype_space( substr( $value, 53, 2 ) ) ){ //Kfz-Typen sind vorhanden
-            if( substr( $value, 74, 12 ) && !ctype_space( substr( $value, 74, 8 ) ) ){ //sind Kfz-Typen zu weit nach lins verschoben?
-                $posBegin = strposArray( $value, $carTypesArray, 74 );
-                if( $posBegin === false || $posBegin >= 84 ) continue;
-                $posEnd = strpos( $value, '   ', $posBegin );
-                $length = $posEnd - $posBegin;
-                $stringToShift = substr( $value, $posBegin, $length );
-                $replaceStr = str_repeat( ' ', $length );
-                $value = substr_replace( $value, $replaceStr, $posBegin, $length );
-                $value = substr_replace( $value, $stringToShift, 84, $length );
-                if( $debug ) $formatedLines[$key] = substr( $value, 52, 58 );
-                $allLines[$key] = $value;
-                if( $debug ) echo "posBegin: ".$posBegin." PosEnd: ".$posEnd." String: ".$stringToShift." \n";
-            }
+        if( substr( $value, 74, 12 ) && !ctype_space( substr( $value, 74, 8 ) ) ){ //sind Kfz-Typen zu weit nach lins verschoben?
+            $posBegin = strposArray( $value, $carTypesArray, 74 );
+            if( $posBegin === false || $posBegin >= 84 ) continue;
+            $posEnd = strpos( $value, '   ', $posBegin );
+            $length = $posEnd - $posBegin;
+            $stringToShift = substr( $value, $posBegin, $length );
+            $replaceStr = str_repeat( ' ', $length );
+            $value = substr_replace( $value, $replaceStr, $posBegin, $length );
+            $value = substr_replace( $value, $stringToShift, 84, $length );
+            if( $debug ) $formatedLines[$key] = substr( $value, 52, 58 );
+            $allLines[$key] = $value;
+            if( $debug ) echo "posBegin: ".$posBegin." PosEnd: ".$posEnd." String: ".$stringToShift." \n";
         }
+        //}
         //if( !ctype_space( substr( $value, 53, 2 ) ) ) $formatedLines[$key] = substr( $value, 52, 45 );//52,32
         //if( !ctype_space( substr( $value, 53, 2 ) ) ) $formatedLines[$key] = substr( $value, 74, 12 );//52,32
         //file_put_contents( 'mykba.txt', implode( PHP_EOL, $value ), FILE_APPEND );
 
-        ////// also der Trick ist gezielt nach Fhz-Typen zu suchen, Fhz-Typen befinden sich in einem Array
-        //////Zum debuggen soll die Position des Zeichens in der Zeile darüber stehen 1..\n  str_split ( string $string , int $length = 1 ) : array dann index zeigen
 
     }
-    file_put_contents('mykba.txt', implode( PHP_EOL, $allLines ) );
+    $posSeperator = array( 13, 24, 54, 87, 123, 151, 170, 179, 190, 198, 215, 225, 235, 243 );
+    foreach( $allLines as $key => $value ){
+        foreach( $posSeperator as $posKey => $posValue ){
+            $value = substr_replace( $value, '|', $posValue, 0 );
+        }
+
+        $allLines[$key] = $value;
+    }
+    file_put_contents( 'carskba.csv', implode( PHP_EOL, $allLines ) );
     if( $debug ) file_put_contents('formatedLines.txt', implode( PHP_EOL, $formatedLines ) );
     echo "\n\n";
     //var_dump( $lines );
---
+++ kba/tmp.sql
@@ -0,0 +1,34 @@
+DROP TABLE IF EXISTS carskba;
+CREATE TABLE carskba(
+	hsn TEXT,
+	tsn TEXT,
+	hersteller TEXT,
+	marke TEXT,
+	name TEXT,
+	datum TEXT,
+	klasse TEXT,
+	aufbau TEXT,
+	kraftstoff TEXT,
+	leistung TEXT,
+	hubraum TEXT,
+	achsen TEXT,
+	antrieb TEXT,
+	sitze TEXT,
+	masse TEXT	
+);
+COPY carskba( hsn, tsn, hersteller, marke, name, datum, klasse, aufbau, kraftstoff, leistung,	hubraum, achsen, antrieb, sitze, masse 	)
+FROM '/var/www/kivitendo-crm/lxcars/kba/carskba.csv' DELIMITER '|' CSV;
+
+
+
+DROP TABLE IF EXISTS ksart;
+CREATE TABLE ksart(
+	kstext TEXT,
+	kscode TEXT
+);
+COPY ksart( kstext, kscode ) FROM '/var/www/kivitendo-crm/lxcars/kba/ksart.csv' DELIMITER '|' CSV;
+
+SELECT * FROM carskba LEFT JOIN ksart ON ( kraftstoff::INT = kscode::INT ) WHERE hsn ILIKE '0600%' AND tsn ILIKE '%300%';
+
+SELECT DISTINCT TRIM(klasse) FROM carskba;
+SELECT * FROM carskba WHERE klasse ILIKE '%11%';
