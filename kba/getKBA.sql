1856f4b3e078e209dba432bcbe7b7dd0dc870e1b
Ronny Zimmermann <ronny@inter-data.de>
2021-06-13 08:02:37 +0200

    Admin-File hinzugefügt

--- kba/getKBA.sql
+++ kba/getKBA.sql
@@ -1,30 +1,29 @@
 DROP TABLE IF EXISTS carskba;
 CREATE TABLE carskba(
-	hsn TEXT,
-	tsn TEXT,
-	hersteller TEXT,
-	marke TEXT,
-	name TEXT,
-	datum TEXT,
-	klasse TEXT,
-	aufbau TEXT,
-	kraftstoff TEXT,
-	leistung TEXT,
-	hubraum TEXT,
-	achsen TEXT,
-	antrieb TEXT,
-	sitze TEXT,
-	masse TEXT	
+    hsn TEXT,
+    tsn TEXT,
+    hersteller TEXT,
+    marke TEXT,
+    name TEXT,
+    datum TEXT,
+    klasse TEXT,
+    aufbau TEXT,
+    kraftstoff TEXT,
+    leistung TEXT,
+    hubraum TEXT,
+    achsen TEXT,
+    antrieb TEXT,
+    sitze TEXT,
+    masse TEXT
 );
-COPY carskba( hsn, tsn, hersteller, marke, name, datum, klasse, aufbau, kraftstoff, leistung,	hubraum, achsen, antrieb, sitze, masse 	)
+COPY carskba( hsn, tsn, hersteller, marke, name, datum, klasse, aufbau, kraftstoff, leistung,    hubraum, achsen, antrieb, sitze, masse )
 FROM '/var/www/kivitendo-crm/lxcars/kba/carskba.csv' DELIMITER '|' CSV;
-
 
 
 DROP TABLE IF EXISTS ksart;
 CREATE TABLE ksart(
-	kstext TEXT,
-	kscode TEXT
+    kstext TEXT,
+    kscode TEXT
 );
 COPY ksart( kstext, kscode ) FROM '/var/www/kivitendo-crm/lxcars/kba/ksart.csv' DELIMITER '|' CSV;
 
