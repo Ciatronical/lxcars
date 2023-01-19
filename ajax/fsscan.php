9a0f556496b34ba1fd4d6e6be313eb81209d2ff6
Ronny Zimmermann <ronny@inter-data.de>
2023-01-19 22:45:21 +0100

    Fahrzeugscheinscanner: Erste Fahrzeugdaten werden dargestellt

--- ajax/fsscan.php
+++ ajax/fsscan.php
@@ -1,10 +1,17 @@
 <?php
-//require_once __DIR__.'/../../inc/stdLib.php'; // for debug
-require_once __DIR__.'/../inc/ajax2function.php';
+    require_once __DIR__.'/../../inc/stdLib.php'; // for debug
+    require_once __DIR__.'/../inc/ajax2function.php';
 
-function getScans( $data ){
-    $apiKeyArray = getDefaultsByArray( array( 'lxcarsapi') );
-    echo file_get_contents( 'https://fahrzeugschein-scanner.de/api/Scans/GetScans/'.$apiKeyArray[ 'lxcarsapi' ].'/?take='.$data['fsmax'] );
-}
+    function getScans( $data ){
+        $apiKeyArray = getDefaultsByArray( array( 'lxcarsapi') );
+        echo file_get_contents( 'https://fahrzeugschein-scanner.de/api/Scans/GetScans/'.$apiKeyArray['lxcarsapi'].'/?take='.$data['fsmax'] );
+    }
+
+    function getFsData( $data ){
+        //echo json_encode( 'hello world '.$data['id'] );// /api/Scans/ScanDetails/{accessKey}/{scanId}/{getImageCuts};
+        $apiKeyArray = getDefaultsByArray( array( 'lxcarsapi') );
+        writeLog( 'https://fahrzeugschein-scanner.de/api/Scans/ScanDetails/'.$apiKeyArray['lxcarsapi'].'/'.$data['id'].'/false' );
+        echo file_get_contents( 'https://fahrzeugschein-scanner.de/api/Scans/ScanDetails/'.$apiKeyArray['lxcarsapi'].'/'.$data['id'].'/true' );
+    }
 
 ?>
