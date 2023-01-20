6804111252a826a7a0ef974340bd6657997e04f0
Ronny Zimmermann <ronny@inter-data.de>
2023-01-20 07:55:52 +0100

    Fahrzeugscheinscanner: getDocument() begonnen

--- ajax/fsscan.php
+++ ajax/fsscan.php
@@ -8,10 +8,23 @@
     }
 
     function getFsData( $data ){
-        //echo json_encode( 'hello world '.$data['id'] );// /api/Scans/ScanDetails/{accessKey}/{scanId}/{getImageCuts};
         $apiKeyArray = getDefaultsByArray( array( 'lxcarsapi') );
-        writeLog( 'https://fahrzeugschein-scanner.de/api/Scans/ScanDetails/'.$apiKeyArray['lxcarsapi'].'/'.$data['id'].'/false' );
         echo file_get_contents( 'https://fahrzeugschein-scanner.de/api/Scans/ScanDetails/'.$apiKeyArray['lxcarsapi'].'/'.$data['id'].'/true' );
+    }
+
+    function getDocument( $data ){
+        $apiKeyArray = getDefaultsByArray( array( 'lxcarsapi') );
+        writeLog( 'https://fahrzeugschein-scanner.de/api/Scans/Document/'.$apiKeyArray['lxcarsapi'].'/'.$data['id'] );
+        //echo file_get_contents( 'https://fahrzeugschein-scanner.de/api/Scans/ScanDetails/'.$apiKeyArray['lxcarsapi'].'/'.$data['id'].'/true' );
+        /*
+            if (file_put_contents($file_name, file_get_contents($url)))
+    {
+        echo "File downloaded successfully";
+    }
+    else
+    {
+        echo "File downloading failed.";
+    }*/
     }
 
 ?>
